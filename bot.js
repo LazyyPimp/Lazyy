f55272dd2a4a7158b7edf95a3cd5d3ebd0e3fca5
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 09:09:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,11 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    // set a "now playing" message (optional)
+    client.user.setActivity("Now Playing SOMETHING");
+	
+	// set the bot's online/idle/dnd/invisible status
+	client.user.setStatus("online");
 });
     
 client.on('message', message => {
