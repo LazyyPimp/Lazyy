5ba814e9298ec17153c453840ce437ed53026403
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 09:17:52 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,15 +12,6 @@ client.on('message', message => {
   	}
 });
 
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
   if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
     
