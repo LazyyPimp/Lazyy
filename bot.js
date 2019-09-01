b25d0d037ce3db9e589a7f18d88a04d25e2bcabd
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 11:30:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,24 +48,22 @@ client.on("message", async message => {
         msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
     }
      
-    if(command === "ban") {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+    if (cmd === "magic") {
+		let msg = "https://media.giphy.com/media/5ftsmLIqktHQA/giphy.gif";
+		msg += "\n Ah ah ah! You didn't say the magic word.";
+		channel.send(msg);
+	}
+	if (cmd === "disco") {
+		const discos = [
+			':beers: :tropical_drink: **P A R T Y** :man_dancing: :cartwheel: ',
+			':palm_tree: :cocktail: **P A R T Y** :slot_machine: :lifter: ',
+			':handball: :dancer:  **P A R T Y** :beer: :100: '
+		];
+		let n = Math.floor(Math.random() * (discos.length - 1)) + 1;
+		channel.send( discos[n] );
+		message.delete(250).then(msg => console.log(`Deleted message from ${msg.author.username}: ${msg.content}`)).catch(console.error);
+		pointsReward = 2;
 
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
 
     if (cmd === "say") {
