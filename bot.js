2c4da498a319b0128aae87f294ac5d8a8bfab457
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 09:22:34 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-client.user.setStatus("idle");
-});
+// When the bot's online, what's in these brackets will be executed
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+
+    // Set the user presence
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "me getting developed",
+            type: "WATCHING"
+        }
+    }); 
+})
     
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
-
-  if(command === "purge") {
-    // This command removes all messages from all users in the channel, up to 100.
-    
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
