b5182b4bc80c9f057fabc8372cc8bf375e65662a
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 09:26:42 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,5 +21,50 @@ client.on('message', message => {
   	}
 });
 
+client.on("message", async message => {
+    const prefix = "_";
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === "ping") {
+        // Send a message
+        const msg = await message.channel.send(`🏓 Pinging....`);
+
+        // Edit the message
+        msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
+    }
+
+    if (cmd === "say") {
+        // Check if you can delete the message
+        if (message.deletable) message.delete();
+
+        if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
+        
+        // Role color
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
+                .setTimestamp()
+                .setImage(client.user.displayAvatarURL)
+                .setAuthor(message.author.username, message.author.displayAvatarURL);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+    }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
