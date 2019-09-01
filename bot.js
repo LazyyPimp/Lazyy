8634bc98a42262d7654c6826f00c86106f27b47a
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 15:03:29 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -147,12 +147,18 @@ message.reply(list[rand])
     };
 	
 	if (cmd === "serverinfo") {
-	    const embed = new Discord.RichEmbed()
+		function checkDays(date) {
+        let now = new Date();
+        let diff = now.getTime() - date.getTime();
+        let days = Math.floor(diff / 86400000);
+        return days + (days == 1 ? " day" : " days") + " ago";
+    };
+    let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+	 const embed = new Discord.RichEmbed()
         .setAuthor(message.guild.name, message.guild.iconURL)
         .addField("Name", message.guild.name, true)
         .addField("ID", message.guild.id, true)
         .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
-        .addField("Region", region[message.guild.region], true)
         .addField("Total | Humans | Bots", `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}`, true)
         .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)
         .addField("Channels", message.guild.channels.size, true)
