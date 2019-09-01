047c30cd4dc5e9c75783ce4c1aeea57e644c647e
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 10:48:38 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,11 +47,21 @@ client.on("message", async message => {
         // Edit the message
         msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
     }
+    if(command === "purge") {
+    // This command removes all messages from all users in the channel, up to 100.
     
-    const swearWords = ["nigger", "no", "lazyy", "hello"];
-if( swearWords.some(word => message.content.includes(word)) ) {
-  message.delete();
-}
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
 
     if (cmd === "say") {
         // Check if you can delete the message
